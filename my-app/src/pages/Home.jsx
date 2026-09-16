ebb460cc1121d33e544d2af907706a4ae409cb14
puripumjit <pumjit_p@su.ac.th>
2026-09-16 02:42:07 +0000

    lab week7

--- my-app/src/pages/Home.jsx
+++ my-app/src/pages/Home.jsx
@@ -1,8 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { movies } from "../data";
+import MovieCard from "../MovieCard";
+import MovieGallery from "../MovieGallery";
+
 function Home() {
   return (
     <div className="p-8 text-center">
       <h1 className="text-3xl font-bold text-slate-800">🏠 หน้าแรก</h1>
       <p className="mt-2 text-slate-500">ยินดีต้อนรับสู่ MovieHub</p>
+
+      <div className="mx-auto mt-8 max-w-5xl">
+        {movies.slice(0,3).map(m =>(
+          <MovieCard key={m.id}
+            title={m.title}
+            year={m.year}/>
+        ))}
+        
+      </div>
     </div>
   );
 }
